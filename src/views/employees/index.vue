84c98e1b8429ceed5e48fd6438fe67edda0ddc8b
linjiexin <770471223@qq.com>
2020-12-16 21:14:24 +0800

    补全上一次没提交的枚举文件;合适入职时间

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -20,7 +20,7 @@
           <el-table-column label="工号" prop="workNumber" sortable="" />
           <el-table-column label="聘用形式" prop="formOfEmployment" sortable="" :formatter="formatEmployment" />
           <el-table-column label="部门" prop="departmentName" sortable="" />
-          <el-table-column label="入职时间" prop="timeOfEntry" sortable="" />
+          <el-table-column label="入职时间" prop="timeOfEntry" sortable="" :formatter="formatTimeOfEntry" />
           <el-table-column label="账户状态" prop="enableState" sortable="" />
           <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template>
@@ -55,10 +55,7 @@ export default {
   data() {
     return {
       list: [
-        { name: '王大锤' },
-        { name: '陈翠花' },
-        { name: 'Tom' },
-        { name: 'Jerry' }
+
       ],
       pageSetting: {
         page: 1,
@@ -90,6 +87,10 @@ export default {
       const obj = EmployeeEnum.hireType.find(item => item.id === cellValue)
       // 防御性编程，如过得到的对象不存在，就显示为不存在。
       return obj ? obj.value : '不存在'
+    },
+    formatTimeOfEntry(row, column, cellValue, index) {
+      // 更具t截取时间字符串，返回，如果不存在则显示不存在
+      return cellValue.split('T')[0] || '不存在'
     }
   }
 
