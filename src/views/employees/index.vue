1e3d580b5d8b6c644d82face71174fdc769cc114
linjiexin <770471223@qq.com>
2020-12-16 14:30:59 +0800

    使用插槽调用时自定义数据

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <PageTools />
+      <PageTools :show-before="true">
+        <span slot="before">左边数据</span>
+        <span slot="after">导出员工</span>
+      </PageTools>
       <h2>
         员工
       </h2>
