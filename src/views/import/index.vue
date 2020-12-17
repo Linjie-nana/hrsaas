967d717fc478a4cf6f4ce9b456efde1d915484d2
linjiexin <770471223@qq.com>
2020-12-17 16:05:54 +0800

    导入文件功能:安装xlsx，添加路由和配置文件

---
+++ src/views/import/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <UploadExcel :on-success="onSuccess" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSuccess({ header, results }) {
+      // 这个函数是excel文件解析完毕之后触发的
+      // 可以获取到表头, 可以获取到数据行
+      console.log(header)
+      console.log(results)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
