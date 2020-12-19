5638cb2c4cf946fa12e4f8334a6d13f4cf67fc55
linjiexin <770471223@qq.com>
2020-12-19 22:00:12 +0800

    图片上传初步封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
 // 创建一个全局组件
 import PageTools from '@/components/PageTools'
 import UploadExcel from './UploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     // 注册导入excel组件
     Vue.component('UploadExcel', UploadExcel)
+    // 注册图片上传
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
