60dc097adc9536b486bd024df1efc331baa5ff29
linjiexin <770471223@qq.com>
2020-12-22 15:40:57 +0800

    权限页面布局和获取数据

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限
-      </h2>
+      <PageTools>
+        <template slot="after">
+          <el-button type="primary">新增权限</el-button>
+        </template>
+      </PageTools>
+      <el-card>
+        <el-table border :data="permissionList" row-key="id" :default-expand-all="true">
+          <el-table-column label="权限名称" prop="name" width="160px" />
+          <el-table-column align="center" label="权限标识" prop="code" />
+          <el-table-column align="center" label="权限描述" prop="description" />
+          <el-table-column align="center" label="操作">
+            <template>
+              <el-button type="text">添加</el-button>
+              <el-button type="text">编辑</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+
     </div>
   </div>
 </template>
 
 <script>
+import { converTree } from '@/utils/auth'
+import { getPermissionList } from '@/api/permission'
 export default {
-
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const res = await getPermissionList()
+      console.log(res)
+      this.permissionList = converTree(res, '0')
+    }
+  }
 }
 </script>
 
