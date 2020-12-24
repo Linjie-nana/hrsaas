840232661bc6deba8b640b6d1b0097e054597388
linjiexin <770471223@qq.com>
2020-12-24 12:34:16 +0800

    功能权限校验1.0

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -52,7 +52,7 @@
               <el-button type="text" size="small">调岗</el-button>
               <el-button type="text" size="small">离职</el-button>
               <el-button type="text" size="small" @click="editRole(row.id)">角色</el-button>
-              <el-button type="text" size="small" @click="delEmployee(row.id)">删除</el-button>
+              <el-button :disabled="$store.state.user.userInfo.roles.points.indexOf('point-user-delete')===-1" type="text" size="small" @click="delEmployee(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
